ff1bc29984ba4ecd5e22319f9e150926593c398f
zhbzwayzhawfbyg <zhbzwayzhawfbyg@gmail.com>
2016-09-03 16:13:07 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+	$dbms="mysql";
+	$host="localhost"
+	$dbName="MessagePush;
+	$user="root";
+	$password="root";
+	$dsn="$dbms:host=$host;dbname=$dbName";
+	try
+	{
+		$pdo=new pdo($dsn,$user,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"set names utf8"));
+	}catch(PDOException $e)
+	{
+		die("connect error !".$e->getMessage()."<br>");
+	}
